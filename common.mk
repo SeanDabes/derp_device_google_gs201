b119e2f55cbf2ae9561a17ea28156a53e2ecf1ec
Matt Filetto <matt.filetto@gmail.com>
2023-02-15 02:55:24 -0800

    gs201: Enable adaptive charging feature
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    (cherry picked from commit cdc64b7ae76c84a4e5e5ab768d0def9a9af1dd15)

--- common.mk
+++ common.mk
@@ -320,6 +320,16 @@ PRODUCT_COPY_FILES += \
 # Hardware Info
 include hardware/google/pixel/HardwareInfo/HardwareInfo.mk
 
+# Allow longer timeout for incident report generation in bugreport
+# Overriding in /product partition instead of /vendor intentionally,
+# since it can't be overridden from /vendor.
+PRODUCT_PRODUCT_PROPERTIES += \
+	dumpstate.strict_run=false
+
+# Adaptive charging
+PRODUCT_COPY_FILES += \
+    device/google/gs201/conf/adaptivecharging.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/adaptivecharging.xml
+
 # AiAi Config
 PRODUCT_COPY_FILES += \
     device/google/gs201/allowlist_com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_com.google.android.as.xml
