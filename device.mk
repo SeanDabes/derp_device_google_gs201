edc5216e42a46a35c96b9995d13d58f923090109
Sean Dabes <koboldo81@gmail.com>
2025-10-27 18:11:29 +0100

    Add Pixel Camera

--- device.mk
+++ device.mk
@@ -1025,3 +1025,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 #Viper4Android
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
+# Pixel Cam
+$(call inherit-product, vendor/google/camera/config.mk)
